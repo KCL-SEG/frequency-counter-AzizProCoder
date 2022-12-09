f8d345ceb515836882896d19ecd31a2b301273f1
Abdulaziz Albanawi <abdulaziz.albanawi@kcl.ac.uk>
2022-12-09 15:38:06 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+
     frequencies = {}
-    # Your code goes here
+
+    for i in items:
+
+        if str(i) in frequencies:
+
+            frequencies[str(i)] += 1
+
+        else:
+
+            frequencies[str(i)] = 1
+
     return frequencies
+
